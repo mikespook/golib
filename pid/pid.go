8428d79ee824525a8d766f980b5b643b3e05f2f2
mikespook <mikespook@gmail.com>
2012-04-06 23:28:24 +0800

    Promoted the pidfile.

--- pid/pid.go
+++ pid/pid.go
@@ -5,24 +5,26 @@ import (
     "strconv"
 )
 
-type PidFile string
+type PidFile struct {
+    path string
+    Pid int
+}
 
-func New(path string) (pf PidFile, err error) {
-    pf = PidFile(path)
-    f, err := os.OpenFile(pf.String(), os.O_CREATE | os.O_EXCL | os.O_WRONLY, 0600)
+func New(path string) (pf *PidFile, err error) {
+    pf = &PidFile{path, os.Getpid()}
+    f, err := os.OpenFile(pf.path, os.O_CREATE | os.O_EXCL | os.O_WRONLY, 0600)
     if err != nil {
         return
     }
     defer f.Close()
-    _, err = f.WriteString(strconv.Itoa(os.Getpid()))
+    _, err = f.WriteString(strconv.Itoa(pf.Pid))
     return
 }
 
-func (pf PidFile)Close() error {
-    return os.Remove(pf.String())
+func (pf *PidFile)Close() error {
+    return os.Remove(pf.path)
 }
 
-func (pf PidFile)String() string {
-    return string(pf)
+func (pf *PidFile)String() string {
+    return strconv.Itoa(pf.Pid)
 }
-
