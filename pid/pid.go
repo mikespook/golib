123c55d3f152e2af9dc289dbd309d6bea787cdd4
mikespook <none@none>
2012-05-11 17:34:08 +0800

    test if the process exists(unfinished)

--- pid/pid.go
+++ pid/pid.go
@@ -2,7 +2,12 @@ package pid
 
 import (
     "os"
+    "errors"
     "strconv"
+)
+
+var (
+    ProcessExists = errors.New("Process already exists.")
 )
 
 type PidFile struct {
@@ -12,9 +17,26 @@ type PidFile struct {
 
 func New(path string) (pf *PidFile, err error) {
     pf = &PidFile{path, os.Getpid()}
-    f, err := os.OpenFile(pf.path, os.O_CREATE | os.O_EXCL | os.O_WRONLY, 0600)
+    oldf, err := os.OpenFile(pf.path, os.O_CREATE | os.O_EXCL | os.O_WRONLY, 0600)
     if err != nil {
-        return
+        var pidstr [10]byte
+        n, err := oldf.Read(pidstr[:])
+        if err != nil {
+            return nil, err
+        }
+        pid, err := strconv.Atoi(string(pidstr[:n]))
+        if err != nil {
+            return nil, err
+        }
+        _, err = os.FindProcess(pid)
+        if err == nil {
+            // process exists
+            return nil, ProcessExists
+        }
+    }
+    f, err := os.OpenFile(pf.path, os.O_CREATE | os.O_TRUNC | os.O_WRONLY, 0600)
+    if err != nil {
+        return nil, err
     }
     defer f.Close()
     _, err = f.WriteString(strconv.Itoa(pf.Pid))
--- pid/pid_test.go
+++ pid/pid_test.go
@@ -18,3 +18,17 @@ func TestPidFile(t *testing.T) {
         t.Error(err)
     }
 }
+
+func TestProcessExists(t *testing.T) {
+    pf1, err := New(PIDFILE)
+    if err != nil {
+        t.Error(err)
+    }
+    defer pf1.Close()
+    pf2, err := New(PIDFILE)
+    if err != nil {
+        t.Error(err)
+    } else {
+        defer pf2.Close()
+    }
+}
