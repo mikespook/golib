006b400b2e27161649b25876ee27229e6550c142
mikespook <none@none>
2012-06-12 11:08:53 +0800

    promoted the profiling

--- prof/prof.go
+++ prof/prof.go
@@ -5,16 +5,36 @@ import (
     "runtime/pprof"
 )
 
+var (
+    cpuFile, heapFile *os.File
+)
+
 func Start(fn string) (err error) {
-    f , err := os.Create(fn)
+    cpuFile , err = os.Create(fn)
     if err != nil {
         return
     }
-    pprof.StartCPUProfile(f)
+    pprof.StartCPUProfile(cpuFile)
     return
 }
 
 func Stop() {
     pprof.StopCPUProfile()
+    cpuFile.Close()
 }
 
+func NewDump(fn string) (err error) {
+    heapFile, err = os.Create(fn)
+    if err != nil {
+        return
+    }
+    return
+}
+
+func Dump() {
+    pprof.WriteHeapProfile(heapFile)
+}
+
+func CloseDump() {
+    heapFile.Close()
+}
