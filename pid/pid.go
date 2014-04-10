18387066caad4dd5f6f171759a8aef518955ecba
Xing Xing <mikespook@gmail.com>
2014-04-10 11:47:25 +0800

    fixed os.FindProcess issue

--- pid/pid.go
+++ pid/pid.go
@@ -1,53 +1,54 @@
 package pid
 
 import (
-    "os"
-    "errors"
-    "strconv"
+	"errors"
+	"os"
+	"strconv"
+	"syscall"
 )
 
 var (
-    ProcessExists = errors.New("Process already exists.")
+	ErrProcessExists = errors.New("Process already exists.")
 )
 
 type PidFile struct {
-    path string
-    Pid int
+	path string
+	Pid  int
 }
 
 func New(path string) (pf *PidFile, err error) {
-    pf = &PidFile{path, os.Getpid()}
-    var f *os.File
-    f, err = os.OpenFile(path, os.O_CREATE | os.O_EXCL | os.O_WRONLY, 0600)
-    if err != nil {
-        f, err = os.OpenFile(path, os.O_CREATE | os.O_RDWR, 0600)
-        if err != nil {
-            return
-        }
-        var pidstr [10]byte
-        var n int
-        n, err = f.Read(pidstr[:])
-        if err != nil {
-            return
-        }
-        var pid int
-        pid, err = strconv.Atoi(string(pidstr[:n]))
-        if err != nil {
-            return
-        }
-        _, err = os.FindProcess(pid)
-        if err == nil {
-            // process exists
-            err = ProcessExists
-            return
-        }
-        f.Truncate(int64(n))
-    }
-    defer f.Close()
-    _, err = f.WriteString(strconv.Itoa(pf.Pid))
-    return
+	pf = &PidFile{path, os.Getpid()}
+	var f *os.File
+	f, err = os.OpenFile(path, os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0600)
+	if err != nil { // file exists
+		f, err = os.OpenFile(path, os.O_CREATE|os.O_RDWR, 0600)
+		if err != nil {
+			return
+		}
+		var pidstr [10]byte
+		var n int
+		// read pid
+		n, err = f.Read(pidstr[:])
+		if err != nil {
+			return
+		}
+		var pid int
+		pid, err = strconv.Atoi(string(pidstr[:n]))
+		if err != nil {
+			return
+		}
+		// find pid
+		if err = syscall.Kill(pid, 0); err == nil {
+			err = ErrProcessExists
+			return
+		}
+		f.Truncate(int64(n))
+	}
+	defer f.Close()
+	_, err = f.WriteString(strconv.Itoa(pf.Pid))
+	return
 }
 
-func (pf *PidFile)Close() error {
-    return os.Remove(pf.path)
+func (pf *PidFile) Close() error {
+	return os.Remove(pf.path)
 }
--- pid/pid_test.go
+++ pid/pid_test.go
@@ -1,35 +1,46 @@
 package pid
 
 import (
-    "testing"
+	"os"
+	"testing"
 )
 
 const (
-    PIDFILE = "./test.pid"
+	PIDFILE = "./test.pid"
 )
 
 func TestPidFile(t *testing.T) {
-    pf, err := New(PIDFILE)
-    if err != nil {
-        t.Error(err)
-    }
+	pf, err := New(PIDFILE)
+	if err != nil {
+		t.Error(err)
+	}
 
-    if err := pf.Close(); err != nil {
-        t.Error(err)
-    }
+	if err := pf.Close(); err != nil {
+		t.Error(err)
+	}
 }
 
 func TestProcessExists(t *testing.T) {
-    pf1, err := New(PIDFILE)
-    if err != nil {
-        t.Error(err)
-    }
-    defer pf1.Close()
-    pf2, err := New(PIDFILE)
-    if err != ProcessExists {
-        t.Error("The process should exist.")
-    }
-    if pf2 != nil {
-        defer pf2.Close()
-    }
+	pf1, err := New(PIDFILE)
+	if err != nil {
+		t.Error(err)
+	}
+	defer pf1.Close()
+	pf2, err := New(PIDFILE)
+	if err != ErrProcessExists {
+		t.Error("The process should exist.")
+	}
+	if pf2 != nil {
+		defer pf2.Close()
+	}
+}
+
+func TestPIDFileExists(t *testing.T) {
+	f, _ := os.OpenFile(PIDFILE, os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0600)
+	f.WriteString("99999")
+	pf, err := New(PIDFILE)
+	if err != nil {
+		t.Error(err)
+	}
+	defer pf.Close()
 }
