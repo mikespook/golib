1661c1cf5a8ea9a7a3af75dc4215145aa475593e
Xing Xing <mikespook@gmail.com>
2014-10-16 18:03:42 +0800

    setup the flag after log is inited

--- log/flag.go
+++ log/flag.go
@@ -6,43 +6,45 @@
 package log
 
 import (
-    "flag"
-    "strings"
+	"flag"
+	"strings"
 )
 
 var (
-    LogFile  = flag.String("log", "", "log to write (empty for STDOUT)")
-    LogLevel = flag.String("log-level", "all", "log level " +
-        "('error', 'warning', 'message', 'debug', 'all' and 'none'" +
-        " are combined with '|')")
+	LogFile, LogLevel string
 )
 
 func StrToLevel(str string) int {
-    level := LogNone
-    levels := strings.SplitN(str, "|", -1)
-    for _, v := range levels {
-        switch v {
-        case "none":
-            level = level | LogNone
-            break
-        case "error":
-            level = level | LogError
-        case "warning":
-            level = level | LogWarning
-        case "message":
-            level = level | LogMessage
-        case "debug":
-            level = level | LogDebug
-        case "all":
-            level = LogAll
-        default:
-        }
-    }
+	level := LogNone
+	levels := strings.SplitN(str, "|", -1)
+	for _, v := range levels {
+		switch v {
+		case "none":
+			level = level | LogNone
+			break
+		case "error":
+			level = level | LogError
+		case "warning":
+			level = level | LogWarning
+		case "message":
+			level = level | LogMessage
+		case "debug":
+			level = level | LogDebug
+		case "all":
+			level = LogAll
+		default:
+		}
+	}
 	return level
 }
 
 func InitWithFlag() {
-    if err := Init(*LogFile, StrToLevel(*LogLevel)); err != nil {
-        Error(err)
-    }
+	flag.StringVar(&LogFile, "log", "", "log to write (empty for STDOUT)")
+	flag.StringVar(&LogLevel, "log-level", "all", "log level "+
+		"('error', 'warning', 'message', 'debug', 'all' and 'none'"+
+		" are combined with '|')")
+
+	if err := Init(LogFile, StrToLevel(LogLevel)); err != nil {
+		Error(err)
+	}
 }
