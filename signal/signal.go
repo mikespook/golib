c9616adc6ba58b48970f9c61bf3e41a505935dd2
Xing Xing <mikespook@gmail.com>
2014-12-08 11:38:49 +0800

    go fmt

--- signal/signal.go
+++ signal/signal.go
@@ -1,78 +1,76 @@
 package signal
 
 import (
-    "os"
-    S "os/signal"
+	"os"
+	S "os/signal"
 )
 
 // return true if need to break
 type Callback func() bool
 
 type Handler struct {
-    schan chan os.Signal
-    cb map[os.Signal]Callback
+	schan chan os.Signal
+	cb    map[os.Signal]Callback
 }
 
 func NewHandler() (sh *Handler) {
-    sh = &Handler{
-        schan: make(chan os.Signal),
-        cb: make(map[os.Signal]Callback, 5),
-    }
-    return
+	sh = &Handler{
+		schan: make(chan os.Signal),
+		cb:    make(map[os.Signal]Callback, 5),
+	}
+	return
 }
 
-func (sh *Handler)Bind(s os.Signal, cb Callback) {
-    S.Notify(sh.schan, s)
-    sh.cb[s] = cb
+func (sh *Handler) Bind(s os.Signal, cb Callback) {
+	S.Notify(sh.schan, s)
+	sh.cb[s] = cb
 }
 
 func (sh *Handler) Unbind(s os.Signal) {
-    delete(sh.cb, s)
+	delete(sh.cb, s)
 }
 
-func (sh *Handler)Loop() os.Signal {
-    for s := range sh.schan {
-        if f, ok := sh.cb[s]; ok {
-            if f() {
-	            return s
+func (sh *Handler) Loop() os.Signal {
+	for s := range sh.schan {
+		if f, ok := sh.cb[s]; ok && f != nil {
+			if f() {
+				return s
 			}
-        }
-    }
-    return nil
+		}
+	}
+	return nil
 }
 
 func (sh *Handler) Close() {
 	S.Stop(sh.schan)
-    close(sh.schan)
+	close(sh.schan)
 }
 
-
 var (
-    DefaultHandler = NewHandler()
+	DefaultHandler = NewHandler()
 )
 
 func Bind(s os.Signal, cb Callback) {
-    DefaultHandler.Bind(s, cb)
+	DefaultHandler.Bind(s, cb)
 }
 
 func Unbind(s os.Signal) {
-    DefaultHandler.Unbind(s)
+	DefaultHandler.Unbind(s)
 }
 
-
 func Loop() os.Signal {
-    return DefaultHandler.Loop()
+	return DefaultHandler.Loop()
 }
 
 func Send(pid int, signal os.Signal) error {
-    proc, err := os.FindProcess(pid)
-    if err != nil {
-        return err
-    }
-    if err := proc.Signal(signal); err != nil {
-        return err
-    }
-    return nil
+	proc, err := os.FindProcess(pid)
+	if err != nil {
+		return err
+	}
+	if err := proc.Signal(signal); err != nil {
+		return err
+	}
+	return nil
 }
 
 func Close() {
